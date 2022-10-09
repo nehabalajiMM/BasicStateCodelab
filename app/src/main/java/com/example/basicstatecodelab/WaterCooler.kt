8c6c8d9e8ddc1d538e169e326bc84fcf427e0b97
nehabalaji <neha.balaji@mutualmobile.com>
2022-10-09 20:54:31 +0530

    add conditions

--- app/src/main/java/com/example/basicstatecodelab/WaterCooler.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCooler.kt
@@ -4,18 +4,20 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
-        Text(
-            text = "You had $count glasses of water."
-        )
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+        var count by remember {mutableStateOf(0)}
+        if (count>0) {
+            Text(
+                text = "You had ${count} glasses of water."
+            )
+        }
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count<10) {
             Text(text = "Add one")
         }
     }
