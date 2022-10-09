f4b7ec35e1e6e1ed081918f4fdddcd5518179501
nehabalaji <neha.balaji@mutualmobile.com>
2022-10-09 20:19:23 +0530

    add button to update count value

---
+++ app/src/main/java/com/example/basicstatecodelab/WaterCooler.kt
@@ -0,0 +1,22 @@
+package com.example.basicstatecodelab.ui.theme
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var count = 0
+        Text(
+            text = "You had $count glasses of water."
+        )
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+            Text(text = "Add one")
+        }
+    }
+}
---
+++ app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -0,0 +1,10 @@
+package com.example.basicstatecodelab
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.basicstatecodelab.ui.theme.WaterCounter
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
+}
