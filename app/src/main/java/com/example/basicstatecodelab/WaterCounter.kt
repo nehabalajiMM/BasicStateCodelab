d0a013b32e2092e59d6deb0c82ce8ff2c52b8194
nehabalaji <neha.balaji@mutualmobile.com>
2022-10-09 22:33:13 +0530

    create stateful and stateless composables

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -5,19 +5,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
+fun StatefulCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember {mutableStateOf(0)}
+        var count by rememberSaveable {mutableStateOf(0)}
+        StatelessCounter(count = count, onIncrement = { count++ }, modifier)
+    }
+}
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
         if (count>0) {
             Text(
                 text = "You had ${count} glasses of water."
             )
         }
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count<10) {
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count<10) {
             Text(text = "Add one")
         }
     }
--- app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -2,9 +2,9 @@ package com.example.basicstatecodelab
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.basicstatecodelab.ui.theme.WaterCounter
+import com.example.basicstatecodelab.ui.theme.StatefulCounter
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    StatefulCounter(modifier)
 }
